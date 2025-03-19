f42dd599c78f244190c8e35dde7ee5273a6803ad
Savva <sav.van.ivan3@mail.ru>
2025-03-19 21:37:45 +0300

    add search

---
+++ src/main/java/servlet/SearchServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import model.User;
+import service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+@WebServlet("/search")
+public class SearchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/pages/Search.jsp").forward(req,resp);
+    }
+
+}
---
+++ src/main/webapp/pages/Search.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Savva
+  Date: 19.03.2025
+  Time: 21:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Search</title>
+</head>
+<body>
+<form action="/userPage" method="get">
+  <input type="text" name="userName" placeholder="userName">
+  <button>sub</button>
+</form>
+</body>
+</html>
